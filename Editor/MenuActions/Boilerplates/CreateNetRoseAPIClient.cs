37a1c1c04300cb1ad0a887a6846c56acbc1e8eb9
Luis Masuelli <luis@datagran.io>
2023-01-27 23:17:41 -0500

    Adding the initial contents of the package

---
+++ Editor/MenuActions/Boilerplates/CreateNetRoseAPIClient.cs
@@ -0,0 +1,146 @@
+using System.Collections.Generic;
+using AlephVault.Unity.Boilerplates.Utils;
+using GameMeanMachine.Unity.NetRose.MenuActions.Boilerplates;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameMeanMachine.Unity.NetRose.Storage
+{
+    namespace MenuActions
+    {
+        namespace Boilerplates
+        {
+            public static class CreateNetRoseAPIClient
+            {
+                /// <summary>
+                ///   Opens a dialog to execute the single-account API client creation boilerplate.
+                /// </summary>
+                [MenuItem("Assets/Create/Net Rose/Boilerplates/Create Single-Account storage API client", false, 205)]
+                public static void ExecuteSingleAccountBoilerplate()
+                {
+                    string directory = "Assets/com.gamemeanmachine.unity.netrose.storage/" +
+                                       "Editor/MenuActions/Boilerplates/Templates";
+                    
+                    TextAsset singleCharAccount = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/SingleCharAccount.cs.txt"
+                    );
+                    TextAsset scope = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Scope.cs.txt"
+                    );
+                    TextAsset map = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Map.cs.txt"
+                    );
+                    TextAsset position = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Position.cs.txt"
+                    );
+                    TextAsset singleCharAccountAPIClient = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/SingleCharAccountAPIClient.cs.txt"
+                    );
+                    new Boilerplate()
+                        .IntoDirectory("Scripts", false)
+                            .IntoDirectory("Server", false)
+                                .IntoDirectory("Authoring", false)
+                                    .IntoDirectory("Behaviours", false)
+                                        .IntoDirectory("External", false)
+                                            .IntoDirectory("Client")
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    singleCharAccountAPIClient, "SingleCharAccountAPIClient", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                            .End()
+                                            .IntoDirectory("Models")
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    singleCharAccount, "SingleCharAccount", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    scope, "Scope", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    map, "Map", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    position, "Position", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                            .End()
+                                        .End()
+                                    .End()
+                                .End()
+                            .End()
+                        .End();
+                }
+                
+                /// <summary>
+                ///   Opens a dialog to execute the multiple-account API client creation boilerplate.
+                /// </summary>
+                [MenuItem("Assets/Create/Net Rose/Boilerplates/Create Multiple-Account storage API client", false, 206)]
+                public static void ExecuteBoilerplate()
+                {
+                    string directory = "Assets/com.gamemeanmachine.unity.netrose.storage/" +
+                                       "Editor/MenuActions/Boilerplates/Templates";
+                    
+                    TextAsset multiCharAccount = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/MultiCharAccount.cs.txt"
+                    );
+                    TextAsset character = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Character.cs.txt"
+                    );
+                    TextAsset scope = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Scope.cs.txt"
+                    );
+                    TextAsset map = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Map.cs.txt"
+                    );
+                    TextAsset position = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/Position.cs.txt"
+                    );
+                    TextAsset multiCharAccountAPIClient = AssetDatabase.LoadAssetAtPath<TextAsset>(
+                        directory + "/MultiCharAccountAPIClient.cs.txt"
+                    );
+                    new Boilerplate()
+                        .IntoDirectory("Scripts", false)
+                            .IntoDirectory("Server", false)
+                                .IntoDirectory("Authoring", false)
+                                    .IntoDirectory("Behaviours", false)
+                                        .IntoDirectory("External", false)
+                                            .IntoDirectory("Client")
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    multiCharAccountAPIClient, "MultiCharAccountAPIClient", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                            .End()
+                                            .IntoDirectory("Models")
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    multiCharAccount, "MultiCharAccount", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    character, "Character", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    scope, "Scope", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    map, "Map", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                                .Do(Boilerplate.InstantiateScriptCodeTemplate(
+                                                    position, "Position", 
+                                                    new Dictionary<string, string>()
+                                                ))
+                                            .End()
+                                        .End()
+                                    .End()
+                                .End()
+                            .End()
+                        .End();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# unity-remote-storage
+This package contains a set of utilities to ease the use of external storage for NetRose.
+
+# Install
+To install this package you need to open the package manager in your project and:
+
+  1. Add a scoped registry with:
+     - "name": "AlephVault"
+     - "url": "https://unity.packages.alephvault.com"
+     - "scopes": ["com.alephvault"]
+  2. And another registry with:
+     - "name": "GameMeanMachine"
+     - "url": "https://unity.packages.gamemeanmachine.com"
+     - "scopes": ["com.gamemeanmachine"]
+  3. Look for this package: `com.gamemeanmachine.unity.netrose.storage`.
+  4. Install it.
+
+# Usage
+
+# Notes
+This documentation has to be updated after the big migration.
